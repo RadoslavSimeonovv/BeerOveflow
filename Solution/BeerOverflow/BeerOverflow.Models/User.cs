6596acd3e00b039c91be6c4635183f7abb698380
Radoslav Simeonov <rsimeonovv@abv.bg>
2020-04-06 21:54:07 +0300

    Added the starting implementation of User class in Models

---
+++ Solution/BeerOverflow/BeerOverflow.Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerOverflow.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+    }
+}
