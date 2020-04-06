cadca9479c837b3fc8170aee125bc97743091895
Boyan Vuchev <bvuchev@gmail.com>
2020-04-07 01:02:17 +0300

    Brewery and country added

---
+++ Solution/BeerOverflow/BeerOverflow.Models/Brewery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerOverflow.Models
+{
+    public class Brewery
+    {
+        public int Id { get; set; }
+        public string BreweryName { get; set; }
+        public int CountryId { get; set; }
+
+    }
+}
