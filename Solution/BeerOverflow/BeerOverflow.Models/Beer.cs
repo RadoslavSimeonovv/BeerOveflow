6c045c497a7033a4f4a8ec0ed12a12ff3d607cfa
Boyan Vuchev <bvuchev@gmail.com>
2020-04-06 23:55:48 +0300

    Beer model add

---
+++ Solution/BeerOverflow/BeerOverflow.Models/Beer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerOverflow.Models
+{
+    public class Beer
+    {
+        public int Id { get; set; }
+        public string BeerName { get; set; }
+        public int TypeId { get; set; }
+        public int BreweryId { get; set; }
+        public float AlcByVol { get; set; }
+        public int OrgnCtryId { get; set; }
+        public string Description { get; set; }
+        public DateTime DateUnlisted { get; set; }
+    }
+}
