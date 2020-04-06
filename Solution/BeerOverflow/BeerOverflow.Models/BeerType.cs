adbdc8fd5c6b83b143bd06852f7a073b05e5e00c
Boyan Vuchev <bvuchev@gmail.com>
2020-04-06 23:56:16 +0300

    Beer type add

---
+++ Solution/BeerOverflow/BeerOverflow.Models/BeerType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerOverflow.Models
+{
+    public class BeerType
+    {
+        public int ID { get; set; }
+        public string Type { get; set; }
+    }
+}
